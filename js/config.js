7e50d27b9a8ca8b726d6cf138b4d242f1682ce94
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-10-20 15:44:00 -0700

    delete inactive configuration paramentrs

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_pKVzRjGlf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7m9bf4dlh59i7eqlo6aluv1v40', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cr891ioq3h.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
